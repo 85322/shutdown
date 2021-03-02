1138666e8db2d8b4e8cccf734112f71d7c086c34
me <4m4zon3@web.de>
2021-03-02 02:19:46 +0100

    basic batch syntax

--- script.bat
+++ script.bat
@@ -5,4 +5,7 @@ ECHO calc 1 value = %ten%
 ECHO calc 2 value = %four%
 ECHO %DATE%
 ECHO Date: %DATE%  Time: %TIME%>> testtext.txt
-PAUSE
+IF EXIST "fi3le.txt" ( 
+    ECHO file.txt found) ELSE ( 
+        ECHO file.txt not found
+    )
