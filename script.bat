1b0e5c17f001d75125e440a39cb2c9652b0bdd3a
me <4m4zon3@web.de>
2021-03-10 03:03:02 +0100

    added several commands

--- script.bat
+++ script.bat
@@ -1,11 +1,20 @@
-@echo off
-SET /A four=1+3
-SET /A ten=5+5
-ECHO calc 1 value = %ten%
-ECHO calc 2 value = %four%
-ECHO %DATE%
-ECHO Date: %DATE%  Time: %TIME%>> testtext.txt
-IF EXIST "fi3le.txt" ( 
-    ECHO file.txt found) ELSE ( 
-        ECHO file.txt not found
-    )
+@ECHO off
+ECHO OPTIONS
+ECHO.
+ECHO [1] = Shutdown
+ECHO [2] = Shutdown in 1 minute
+ECHO [3] = Shutdown in 10 minutes
+ECHO [4] = Shutdown in 30 minutes
+ECHO [5] = Reboot
+ECHO [6] = Hibernate
+ECHO [9] = Shutdown CANCEL
+ECHO.
+SET /p option=Type option number
+IF %option%==1 SHUTDOWN /s /f /t 0
+IF %option%==2 SHUTDOWN /s /t 600
+IF %option%==3 SHUTDOWN /s /t 600
+IF %option%==4 SHUTDOWN /s /t 1800
+IF %option%==5 SHUTDOWN -r -t 10
+IF %option%==6 SHUTDOWN /h
+IF %option%==9 SHUTDOWN /l
+PAUSE
